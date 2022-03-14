cf9b9d057e91f300071d02cc654f2035db814d5b
shivi <100689315+shiviPrajapati@users.noreply.github.com>
2022-03-14 16:25:21 +0530

    Update string3.js

--- string3.js
+++ string3.js
@@ -1,9 +1,10 @@
 export function findMonth(str){
 
-    let month;
+      let month;
 
     var format= /^(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$/;
-    if(!(format.test(str)))
+    var format1= /^(0[1-9]|1[0-2])\/(0[1-9]|1)\/(19|20)\d{2}$/;
+    if(!(format.test(str)) && !(format1.test(str)) )
       return 0;
 
     month=str.split(/[/]/);
