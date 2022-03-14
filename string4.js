28e9e5ee06482090558aaefed1561af3d27e66c5
shivi <100689315+shiviPrajapati@users.noreply.github.com>
2022-03-14 16:12:32 +0530

    Update string4.js

--- string4.js
+++ string4.js
@@ -2,17 +2,20 @@ export function findFullName(nameObject){
 
 
     let fullName;
-    let First= nameObject.first_name;
-    fullName=First.charAt(0).toUpperCase()+First.toLowerCase().slice(1);
-
+    if(nameObject.first_name !== undefined){
+        let First= nameObject.first_name;
+        fullName=First.charAt(0).toUpperCase()+First.toLowerCase().slice(1);
+    }
+        
     if(nameObject.middle_name !== undefined){
         let middle= nameObject.middle_name;
         fullName=fullName+" "+middle.charAt(0).toUpperCase()+middle.toLowerCase().slice(1);
     }
 
-    let last= nameObject.last_name;
-    fullName=fullName+" "+last.charAt(0).toUpperCase()+last.toLowerCase().slice(1);
-
+    if(nameObject.last_name !== undefined){
+        let last= nameObject.last_name;
+        fullName=fullName+" "+last.charAt(0).toUpperCase()+last.toLowerCase().slice(1);
+    }
 
     
     return fullName;
